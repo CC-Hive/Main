908f205351749849718520cef4f344a6a2e59a51
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-09-17 01:06:36 +0100

    needs more humor

---
+++ Code/Crash Quips.lua
@@ -0,0 +1,13 @@
+-- Ideas
+--  http://minecraft.gamepedia.com/Crash#Witty_comments
+--  http://minecraft.gamepedia.com/Splash#Complete_list_of_splashes
+
+-- About
+--  This is a bunch of witty comments which will be prepended to crash logs after the fashion of MineCraft.
+--  For ease the are stored as a lua table.
+
+local quips = {
+  "Lets hope that the crash reporter is fine.",
+  "You were going to see this eventually.",
+  "SupercalifragilisticCRASHidocious!",
+}
