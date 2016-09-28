d4a91ac3d2291fb4d9b31d5ff0635e2d25ec33e6
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-09-28 23:36:22 +0100

    more quips

--- Code/Crash Quips.lua
+++ Code/Crash Quips.lua
@@ -1,14 +1,20 @@
--- Ideas
---  http://minecraft.gamepedia.com/Crash#Witty_comments
---  http://minecraft.gamepedia.com/Splash#Complete_list_of_splashes
-
 -- About
 --  This is a bunch of witty comments which will be prepended to crash logs after the fashion of MineCraft.
---  For ease the are stored as a lua table.
+--  For ease of use, these are stored as a lua table.
 
 local quips = {
   "Lets hope that the crash reporter is fine.",
   "You were going to see this eventually.",
   "SupercalifragilisticCRASHidocious!",
   "Don't worry, our team of highly trained creepers can fix this.",
+  "100% bug free! Except that one, and those, and... Nevermind.",
+  "Do not point bugs at face.",
+  "Rule #1: it's never my fault.",
+  "Don't shoot the crash reporter.",
+  "Ultimatly its a wetware issue, either the users wetware needs updating or the developers needs to update the software because their wetware wrote it wrong.",
+  "What did the creeper hit?",
+  "Next time it's your turn to crash.",
+  "'ave you tried turning it off 'n' on again?",
+  "Ooh. Pritty.",
+  "505 Quip not found.",
 }
