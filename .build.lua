6ee7d2c75fc62c5b2385675d28515836eda79eec
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-11-05 01:42:20 +0000

    Howl should get installed now

--- .build.lua
+++ .build.lua
@@ -3,9 +3,11 @@ if not howlci then
   error("This file requires howlci, for a self build use howl manually.")
 end
 
+if not shell.run("pastebin run LYAxmSby get 703e2f46ce68c2ca158673ff0ec4208c/Howl.min.lua Howl") then
+  howlci.status("fail","failed to get Howl")
+end
+
 shell.setDir("Code")
-if shell.run("howl") then
-  howlci.status("ok","shell.run is ok")
-else
-  howlci.status("fail","shell.run failed")
+if not shell.run("Howl") then
+  howlci.status("fail","failed to call Howl")
 end
