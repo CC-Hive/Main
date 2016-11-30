d90346f00ed475b61f652f55968b0f33286f3c09
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-11-30 22:30:06 +0000

    fixed now?

--- .build.lua
+++ .build.lua
@@ -3,6 +3,13 @@ if not howlci then
   error("This file requires howlci, for a self build use howl manually.")
 end
 
+-- compied from https://github.com/SquidDev-CC/Howl/blob/954d89371cf3ceeca2b26ec8c690d6540b818a59/.build.lua
+if _HOST then howlci.log("info", "Host: " .. _HOST) end
+if _CC_VERSION then howlci.log("info", "CC Version" .. _CC_VERSION) end
+if _MC_VERSION then howlci.log("info", "MC Version" .. _MC_VERSION) end
+if _LUAJ_VERSION then howlci.log("info", "LuaJ Version " .. _LUAJ_VERSION) end
+
+
 if not shell.run("pastebin run LYAxmSby get 703e2f46ce68c2ca158673ff0ec4208c/Howl.min.lua Howl") then
   howlci.status("fail","failed to get Howl")
 end
