6eefe2cb7b570ae9276046e0ef3a5b74f46e787e
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-11-30 22:46:17 +0000

    still debugging

--- .build.lua
+++ .build.lua
@@ -17,7 +17,7 @@ if _LUAJ_VERSION then howlci.log("info", "LuaJ Version " .. _LUAJ_VERSION) end
 --print("root files")
 --shell.run("ls")
 
-shell.setDir("Code")
+--shell.setDir("Code")
 if not shell.run("Howl.min.lua") then
   howlci.status("fail","failed to call Howl")
 end
