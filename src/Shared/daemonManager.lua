91654b24fc9c91c2bf57f5ae179cf7e956258166
Nephi (AKA Lupus590) <lupussolitarius590@gmail.com>
2019-03-31 04:30:55 +0100

    lots of cleanup

--- src/Shared/daemonManager.lua
+++ src/Shared/daemonManager.lua
@@ -13,17 +13,18 @@ local function add(newdaemonName, newdaemonFunc, replaceIfExists)
     error("Arg[2] expected function, got "..type(newdaemonFunc))
   end
   if replaceIfExists and type(replaceIfExists) ~= "bool" then
-    error("Arg[3] expected bool, got "..type(replaceIfExists))
+    error("Arg[3] expected bool or nil, got "..type(replaceIfExists))
   end
   if not replaceIfExists and daemons[newdaemonName] then
-    error("daemon exists")
+    error("daemon with name "..newdaemonName.." exists - to overwrite set arg[3] to true")
   end
   daemons[newdaemonName]={coroutine=coroutine.create(newdaemonFunc), eventFilter = nil}
-  local ok, returnedValues = table.pack(coroutine.resume(daemons[newdaemonName].coroutine))
-  if not ok then
-    error(table.unpack(returnedValues))
-  end
-  daemons[newdaemonName].eventFilter = returnedValues[1]
+  local returnedValues = table.pack(coroutine.resume(daemons[newdaemonName].coroutine))
+  local ok = table.remove(returnedValues, 1)
+    if not ok then
+      error(table.unpack(returnedValues, 1, returnedValues.n))
+    end
+    daemons[newdaemonName].eventFilter = returnedValues[1]
 end
 
 local function remove(daemonName)
@@ -33,23 +34,27 @@ local function remove(daemonName)
   daemons[daemonName] = nil
 end
 
-local function listdaemons()
+local function getDaemonList()
   local list = {}
   for k,v in pairs(daemons) do
     table.add(list,k) -- users can list them all with ipairs
     list[k]=true -- or index by name to see if it's there
   end
-
+  return list
 end
 
 local function daemonHost()
-  local event = table.pack()
+  local event = table.pack(os.pullEventRaw())
+  if not doLoop then
+    return
+  end
   for k, v in pairs(daemons)
     if coroutine.status(v) == "suspended" then
       if v.eventFilter == nil or v.eventFilter == event[1] then
-        local ok, returnedValues = table.pack(coroutine.resume(daemons[newdaemonName].coroutine))
+        local returnedValues = table.pack(coroutine.resume(daemons[newdaemonName].coroutine, table.unpack(event, 1, event.n)))
+        local ok = table.remove(returnedValues, 1)
         if not ok then
-          error(table.unpack(returnedValues))
+          error(table.unpack(returnedValues, 1, returnedValues.n))
         end
         daemons[newdaemonName].eventFilter = returnedValues[1]
       end
@@ -57,10 +62,25 @@ local function daemonHost()
   end
 end
 
-local daemonManager = {add=add,
-remove=remove,
-listdaemons=listdaemons,
-daemonHost=daemonHost,
+local doLoop = true
+
+local function exitLoop()
+  exitLoop = false
+end
+
+local function enterLoop()
+  while doLoop do
+    daemonHost()
+  end
+end
+
+local daemonManager = 
+  add=add,
+  remove=remove,
+  getDaemonList=getDaemonList,
+  daemonHost=daemonHost,
+  exitLoop=exitLoop,
+  enterLoop=enterLoop,
 }
 
 return daemonManager
